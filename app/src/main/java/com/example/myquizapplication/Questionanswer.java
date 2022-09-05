56cb488052b343e007c9f076244d6270b69fbca5
Sadiq Arbaz <sadiqraza028@gmail.com>
2022-09-06 00:24:34 +0530

    First Commit

---
+++ app/src/main/java/com/example/myquizapplication/Questionanswer.java
@@ -0,0 +1,27 @@
+package com.example.myquizapplication;
+
+public class Questionanswer {
+    public static String question[] ={
+            "1. An operating system (OS) built exclusively for mobile devices such as smartphones, tablets, PDAs, etc. Similar to a standard OS but is relatively simple and light?",
+            "2. It is created by Google, is one of the most commonly installed mobile OS for mobile devices, with support from various device manufacturers. It is an open source OS, which means developers are given access to unlocked hardware to develop new programs.",
+            "3. It is developed by Apple and is distributed exclusively for Apple hardware (iPhone, iPad, etc.). One of its well-known features is its multi-touch gestures",
+            "4. It is developed by Microsoft Corporation. Some of its features include apps that update real-time, and full support for Microsoft products such as MS Office",
+            "5.  It is a discontinued OS last used by Nokia for their smartphones because they make up the lesser market of mobile operating systems"
+    };
+    public static String choices[][] = {
+            {"Windows Operating System","Linus Operating System","Macintosh Operating System","Mobile Operating System"},
+            {"Android","Windows","Linux","Xamarin"},
+            {"ios(iphone OS)","Windows OS","Android OS","Blackberry OS"},
+            {"Iphone OS","Symbian OS","Blackberry OS","Windows Phone OS"},
+            {"Blackberry OS","Windows OS","Symbian OS","Chrome OS"}
+    };
+
+    public static String correctAnswers[] = {
+            "Mobile Operating System",
+            "Android",
+            "ios(iphone OS)",
+            "Windows Phone OS",
+            "Symbian OS"
+    };
+
+}
